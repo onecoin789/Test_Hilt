1dad03d5f403af8fa4e06f5b6c3d0fc0acf03145
onecoin <one_coin@naver.com>
2024-05-13 20:02:20 +0900

    add ViewPagerAdapter(Fragment2, Fragment3에 각 fragment 넣어주시면 잘 작동합니다!)

--- app/src/main/java/com/example/teamproject_11/MainActivity.kt
+++ app/src/main/java/com/example/teamproject_11/MainActivity.kt
@@ -2,17 +2,48 @@ package com.example.teamproject_11
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.example.teamproject_11.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
 
-    private val bidning by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(bidning.root)
+        setContentView(binding.root)
+
+        initViewPager()
 
     }
+
+    private fun initViewPager() {
+        var viewPager = binding.mainViewPager
+        var tabLayout = binding.tablayout
+        val tabIconArray = arrayOf(
+            R.drawable.home,
+            R.drawable.search,
+            R.drawable.video
+        )
+        val tabTitleArray = arrayOf(
+            "Home",
+            "Search",
+            "My Video"
+        )
+
+        viewPager.adapter = ViewPagerAdapter(this)
+
+        TabLayoutMediator(tabLayout,viewPager) { tab, position ->
+            tab.icon = getDrawable(tabIconArray[position])
+            tab.text = tabTitleArray[position]
+        }.attach()
+        binding.tablayout.tabTextColors =
+            ContextCompat.getColorStateList(this@MainActivity, R.color.tab_select_color)
+        binding.tablayout.tabIconTint =
+            ContextCompat.getColorStateList(this@MainActivity, R.color.tab_select_color)
+    }
 }
+
 
