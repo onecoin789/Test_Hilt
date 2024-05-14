3ea3d3249a227e20e89a91cc974b06e7329f68a4
JinHyungJun <132827490+JinHyungJunHongik@users.noreply.github.com>
2024-05-14 15:17:32 +0900

    더미데이터 추가

--- app/src/main/java/com/example/teamproject_11/YouTubeResponse.kt
+++ app/src/main/java/com/example/teamproject_11/YouTubeResponse.kt
@@ -29,7 +29,7 @@ data class YouTubeVideo(
 
 //items의 속성 중 snippet에 해당하는 오브젝트 클래스, snippet은 해당 item의 업로드 시간 및 제목, 영상 설명 등의 속성을 가지고 있음
 data class Snippet(
-    val publishedAt: Date?,
+    val publishedAt: String?, //임시로 Date 타입에서 String으로 바꿨습니다
     val channelId : String?,
     val title : String?,
     val description : String?,
@@ -48,4 +48,20 @@ data class Key(
     val width: Int?,
     val height: Int?,
 )
+
+//더미 데이터
+
+val dummyData = listOf(
+    YouTubeVideo(null, null, "1", Snippet("2023-05-11", null, "ABIGAIL","Detail Summary", Thumbnails(Key("1",0,0)),null,"MOVIE")),
+    YouTubeVideo(null, null, "2", Snippet("2020-03-14", null, "ACCIDENTIAL TAXAN","Detail Summary", Thumbnails(Key("2",0,0)),null,"MOVIE")),
+    YouTubeVideo(null, null, "3", Snippet("2003-11-21", null, "ALL SAINT DAY","Detail Summary", Thumbnails(Key("3",0,0)),null,"MOVIE")),
+    YouTubeVideo(null, null, "4", Snippet("2011-07-12", null, "NILPHAS","Detail Summary", Thumbnails(Key("4",0,0)),null,"MOVIE")),
+    YouTubeVideo(null, null, "5", Snippet("2018-08-13", null, "AMERICAN DREAMER","Detail Summary", Thumbnails(Key("5",0,0)),null,"MOVIE")),
+    YouTubeVideo(null, null, "6", Snippet("2019-09-12", null, "AMERICAN SOCIETY","Detail Summary", Thumbnails(Key("6",0,0)),null,"MOVIE")),
+    YouTubeVideo(null, null, "7", Snippet("2022-12-11", null, "AMERICAN STAR","Detail Summary", Thumbnails(Key("7",0,0)),null,"MOVIE")),
+    YouTubeVideo(null, null, "8", Snippet("2010-07-18", null, "ARGYLLE","Detail Summary", Thumbnails(Key("8",0,0)),null,"MOVIE")),
+    YouTubeVideo(null, null, "9", Snippet("2016-10-31", null, "ATLAS","Detail Summary", Thumbnails(Key("9",0,0)),null,"MOVIE")),
+)
+
+
 
