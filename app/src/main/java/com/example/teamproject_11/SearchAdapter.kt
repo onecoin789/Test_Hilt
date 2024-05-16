334885083b64a3d531e9430dbf48ea269c0c108e
배기정 <ligerbae@gmail.com>
2024-05-16 13:43:29 +0900

    서치 어댑터, 프래그먼트

---
+++ app/src/main/java/com/example/teamproject_11/SearchAdapter.kt
@@ -0,0 +1,56 @@
+package com.example.teamproject_11
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+
+class SearchAdapter(
+    val items: List<YouTubeResponse>,
+    private var itemClickListener: OnItemClickListener
+) :
+    RecyclerView.Adapter<SearchAdapter.ViewHolder>() {
+
+    interface OnItemClickListener {
+        fun onClick(view: View, position: Int)
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val tvTitle: TextView = itemView.findViewById(R.id.tvTitle)
+        private val tvDate: TextView = itemView.findViewById(R.id.tvDate)
+
+        fun bind(item: YouTubeResponse) {
+            itemView.apply {
+                Glide.with(context)
+                    .load(item.items?.firstOrNull()?.snippet?.thumbnails)
+                    .into(this as ImageView)
+
+                tvTitle.text = item.items?.firstOrNull()?.snippet?.title ?: "No title available"
+                tvDate.text = item.items?.firstOrNull()?.snippet?.publishedAt ?: "No date available"
+
+                setOnClickListener {
+                    val position = adapterPosition
+                    if (position != RecyclerView.NO_POSITION) {
+                        itemClickListener.onClick(it, position)
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_search, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(items[position])
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+}
--- app/src/main/java/com/example/teamproject_11/SearchFragment.kt
+++ app/src/main/java/com/example/teamproject_11/SearchFragment.kt
@@ -1,59 +1,72 @@
 package com.example.teamproject_11
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.view.*
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.teamproject_11.databinding.FragmentSearchBinding
+import com.google.gson.Gson
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SearchFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SearchFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentSearchBinding
+    private lateinit var searchAdapter: SearchAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
+        savedInstanceState: Bundle?,
+    ): View {
+        binding = FragmentSearchBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SearchFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SearchFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // SearchAdapter 초기화
+        searchAdapter = SearchAdapter(emptyList(), object : SearchAdapter.OnItemClickListener {
+            // 아이템 클릭시 DetailActivity intent 전달
+            override fun onClick(view: View, position: Int) {
+                val selectedItem = searchAdapter.items[position]
+                val intent = Intent(context, DetailActivity::class.java)
+                val json = Gson().toJson(selectedItem.items)
+                intent.putExtra("searchQuery", json)
+                startActivity(intent)
             }
+        })
+
+        setRecyclerView()
+        setUpListener()
+    }
+
+    private fun setRecyclerView() {
+        binding.recyclerViewSearch.adapter = searchAdapter
+        binding.recyclerViewSearch.layoutManager = LinearLayoutManager(context)
+    }
+
+    private fun setUpListener() {
+        binding.etSearch.setOnEditorActionListener { _, actionId, _ ->
+            hideKeyboard()
+            // 검색어 입력하고 엔터 누르면 검색 실행
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                performSearch()
+                return@setOnEditorActionListener true
+            }
+            false
+        }
+    }
+
+    private fun hideKeyboard() {
+        val imm =
+            view?.context?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(requireView().windowToken, 0)
+    }
+
+    private fun performSearch() {
+        val searchQuery = binding.etSearch.text.toString()
     }
 }
