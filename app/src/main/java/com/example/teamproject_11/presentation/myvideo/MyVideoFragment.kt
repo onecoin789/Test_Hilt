1eee0875898b53ed49752985c280080440042d32
BGJ <134063143+koreabgj@users.noreply.github.com>
2024-05-20 16:16:03 +0900

    Update MyVideoFragment.kt

--- app/src/main/java/com/example/teamproject_11/presentation/myvideo/MyVideoFragment.kt
+++ app/src/main/java/com/example/teamproject_11/presentation/myvideo/MyVideoFragment.kt
@@ -1,25 +1,30 @@
 package com.example.teamproject_11.presentation.myvideo
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
+import com.example.teamproject_11.R
+import com.example.teamproject_11.room.MyListDataBase
+import com.example.teamproject_11.databinding.ActivityMainBinding
 import com.example.teamproject_11.databinding.FragmentMyVideoBinding
 import com.example.teamproject_11.presentation.home.main.HomeViewModel
+import com.example.teamproject_11.presentation.home.main.HomeViewModelFactory
 import com.example.teamproject_11.presentation.home.model.HomeVideoModel
 import com.example.teamproject_11.presentation.main.MainActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 
 class MyVideoFragment : Fragment() {
     private val binding by lazy { FragmentMyVideoBinding.inflate(layoutInflater) }
     private val viewModel by lazy {
-        ViewModelProvider(
-            requireActivity(),
-            HomeViewModel.HomeViewModelFactory()
-        )[HomeViewModel::class.java]
+        ViewModelProvider(requireActivity(), HomeViewModelFactory())[HomeViewModel::class.java]
     }
 
     override fun onCreateView(
@@ -31,25 +36,25 @@ class MyVideoFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-//        initView()
+        initView()
     }
 
 
     //데이터가 갱신될 수 있도록 onResume()을 오버라이딩 해서 해당 리스트 갱신 메소드를 넣었습니다.
-//    override fun onResume() {
-//        super.onResume()
-//        viewModel.getMyVideoList(requireActivity())
-//    }
-//
-//    private fun initView() {
-//        viewModel.getMyVideoList(requireActivity())
-//        viewModel.myVideoList.observe(viewLifecycleOwner) {
-//            binding.myvideoRecyclerview.adapter = MyVideoAdapter(it, object : OnItemClick {
-//                override fun onItemClick(item: HomeVideoModel) {
-//                    (requireActivity() as MainActivity).openVideoDetailFromHome(item)
-//                }
-//            })
-//        }
-//        binding.myvideoRecyclerview.layoutManager = GridLayoutManager(this.context, 3)
-//    }
+    override fun onResume() {
+        super.onResume()
+        viewModel.getMyVideoList(requireActivity())
+    }
+
+    private fun initView() {
+        viewModel.getMyVideoList(requireActivity())
+        viewModel.myVideoList.observe(viewLifecycleOwner){
+            binding.myvideoRecyclerview.adapter = MyVideoAdapter(it, object : OnItemClick{
+                override fun onItemClick(item: HomeVideoModel) {
+                    (requireActivity() as MainActivity).openVideoDetailFromHome(item)
+                }
+            })
+        }
+        binding.myvideoRecyclerview.layoutManager = GridLayoutManager(this.context, 3)
+    }
 }
